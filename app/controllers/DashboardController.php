911b9a487ba83644195968ae9fca570026c73820
sh3ki <shekaigarcia@gmail.com>
2026-04-28 11:47:03 +0800

    Update app/controllers/DashboardController.php

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
 {
     public function index(Request $request): void
     {
-        $this->authorize(['Admin', 'Manager', 'Cashier', 'Store Staff', 'Accountant']);
+        $this->authorizePermission('dashboard.view');
 
         $service = new DashboardService();
         $summary = $service->summary();
